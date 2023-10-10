eb34ea83001ffb1adb76ed9082cd015c73924a50
Gia Thy Le <giathy2843@gmail.com>
2023-10-09 21:29:15 -0700

    changed stuff

--- hw4/ladder.py
+++ hw4/ladder.py
@@ -1,8 +1,6 @@
 def my_step(n):
-    if n == 1:
+    if n == 1 or n == 0:
         return '1 step '
-    elif n == 2:
-        return '2 steps '
     else:
         return my_step(n-1) + my_step(n-2)
 print(my_step(5)) 
--- hw4/main.py
+++ hw4/main.py
@@ -1,10 +1,15 @@
 from print_caps import allcaps
+from log import timestamp
 
 @allcaps
 def greet():
     return "hello World!"
 
+@timestamp
+def hi():
+    print('hi')
+
 def main():
     greet()
-
+    hi()
 main()
